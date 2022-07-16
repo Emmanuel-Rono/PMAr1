6f138540c65ce68129d2cbdd54f081ff099dfac4
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-07-16 21:00:38 +0300

    understanding Recyclerviews

--- app/src/main/java/com/example/pmar1/MainActivity.kt
+++ app/src/main/java/com/example/pmar1/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController=navHostFragment.navController
     }
 }
